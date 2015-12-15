c84ba74934c1b5c01d4dd2301453280b704dced1
Jordi Sastre <sastrej@gmail.com>
2015-12-15 18:04:03 +0100

    Recuperacio backup botiga produccio

---
+++ cache/smarty/compile/c3/6a/25/c36a25ee00b6d9d1e67e5b93b3bcc80f5b9d1ed5.file.javascript.tpl.php
@@ -0,0 +1,101 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2015-12-03 01:01:31
+         compiled from "/var/lib/openshift/55e615f62d5271473f000022/app-root/runtime/repo/themes/javascript.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:54064685455f1b5d4465331-44381340%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'c36a25ee00b6d9d1e67e5b93b3bcc80f5b9d1ed5' => 
+    array (
+      0 => '/var/lib/openshift/55e615f62d5271473f000022/app-root/runtime/repo/themes/javascript.tpl',
+      1 => 1448993191,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '54064685455f1b5d4465331-44381340',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_55f1b5d44e3207_07432283',
+  'variables' => 
+  array (
+    'js_def' => 0,
+    'k' => 0,
+    'def' => 0,
+    'js_files' => 0,
+    'js_uri' => 0,
+    'js_inline' => 0,
+    'inline' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_55f1b5d44e3207_07432283')) {function content_55f1b5d44e3207_07432283($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_replace')) include '/var/lib/openshift/55e615f62d5271473f000022/app-root/runtime/repo/tools/smarty/plugins/modifier.replace.php';
+?>
+<?php if (isset($_smarty_tpl->tpl_vars['js_def']->value)&&is_array($_smarty_tpl->tpl_vars['js_def']->value)&&count($_smarty_tpl->tpl_vars['js_def']->value)) {?>
+<script type="text/javascript">
+<?php  $_smarty_tpl->tpl_vars['def'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['def']->_loop = false;
+ $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['js_def']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['def']->key => $_smarty_tpl->tpl_vars['def']->value) {
+$_smarty_tpl->tpl_vars['def']->_loop = true;
+ $_smarty_tpl->tpl_vars['k']->value = $_smarty_tpl->tpl_vars['def']->key;
+?>
+<?php if (!empty($_smarty_tpl->tpl_vars['k']->value)&&is_string($_smarty_tpl->tpl_vars['k']->value)) {?>
+<?php if (is_bool($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = <?php echo var_export($_smarty_tpl->tpl_vars['def']->value,true);?>
+;
+<?php } elseif (is_int($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = <?php echo intval($_smarty_tpl->tpl_vars['def']->value);?>
+;
+<?php } elseif (is_float($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = <?php echo smarty_modifier_replace(floatval($_smarty_tpl->tpl_vars['def']->value),',','.');?>
+;
+<?php } elseif (is_string($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = '<?php echo strval($_smarty_tpl->tpl_vars['def']->value);?>
+';
+<?php } elseif (is_array($_smarty_tpl->tpl_vars['def']->value)||is_object($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = <?php echo $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_MODIFIER]['json_encode'][0][0]->jsonEncode($_smarty_tpl->tpl_vars['def']->value);?>
+;
+<?php } elseif (is_null($_smarty_tpl->tpl_vars['def']->value)) {?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = null;
+<?php } else { ?>
+var <?php echo $_smarty_tpl->tpl_vars['k']->value;?>
+ = '<?php echo addcslashes($_smarty_tpl->tpl_vars['def']->value,'\'');?>
+';
+<?php }?>
+<?php }?>
+<?php } ?>
+</script>
+<?php }?>
+<?php if (isset($_smarty_tpl->tpl_vars['js_files']->value)&&count($_smarty_tpl->tpl_vars['js_files']->value)) {?>
+<?php  $_smarty_tpl->tpl_vars['js_uri'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['js_uri']->_loop = false;
+ $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['js_files']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['js_uri']->key => $_smarty_tpl->tpl_vars['js_uri']->value) {
+$_smarty_tpl->tpl_vars['js_uri']->_loop = true;
+ $_smarty_tpl->tpl_vars['k']->value = $_smarty_tpl->tpl_vars['js_uri']->key;
+?>
+<script type="text/javascript" src="<?php echo $_smarty_tpl->tpl_vars['js_uri']->value;?>
+"></script>
+<?php } ?>
+<?php }?>
+<?php if (isset($_smarty_tpl->tpl_vars['js_inline']->value)&&count($_smarty_tpl->tpl_vars['js_inline']->value)) {?>
+<script type="text/javascript">
+<?php  $_smarty_tpl->tpl_vars['inline'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['inline']->_loop = false;
+ $_smarty_tpl->tpl_vars['k'] = new Smarty_Variable;
+ $_from = $_smarty_tpl->tpl_vars['js_inline']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['inline']->key => $_smarty_tpl->tpl_vars['inline']->value) {
+$_smarty_tpl->tpl_vars['inline']->_loop = true;
+ $_smarty_tpl->tpl_vars['k']->value = $_smarty_tpl->tpl_vars['inline']->key;
+?>
+<?php echo $_smarty_tpl->tpl_vars['inline']->value;?>
+
+<?php } ?>
+</script>
+<?php }?><?php }} ?>
